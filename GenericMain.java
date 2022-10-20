5cd19ae223a3a4e2e64d1ba97c10e09f0199fbaf
dsdhumale <d.sdhumale@gmail.com>
2022-10-21 04:54:43 +0530

    Added : Welcome message

---
+++ GenericMain.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz.Day13;
+
+public class GenericMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome in program to find out maximum number and name");
+	}
+}
